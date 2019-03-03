b3c6d4e27f2e6bd4eca42540ebd7b4cf3c6b0830
jinchaolu <30065376+jinchaolu@users.noreply.github.com>
2019-03-03 05:11:54 +0000

    refactor: Change decision making algorithm
    Calculate the offset from the center of the ball to the center of
    the picture then normalize it and multiply with magic number to
    turn the robot

--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -53,18 +53,19 @@ void process_image_callback(const sensor_msgs::Image img)
     // Initial white pixel counter vector (in step direction)
     // Base on the range of left, forward, right (unit: pixel)
     // Find the start, end index of each range in the vector
-    unsigned int height = img.height;
-    unsigned int step = img.step;
+    int height = img.height;
+    int step = img.step;
+
+/*
+    int vec_cout[step] = {};
     
-    unsigned int vec_cout[step] = {};
     unsigned long cout_left    = 0;
     unsigned long cout_forward = 0;
     unsigned long cout_right   = 0;
     
     unsigned int left_range  = (unsigned int)((float)step / 3.0 + 1);
     unsigned int right_range = (unsigned int)((float)step / 3.0 + 1);
-
-/*    
+    
     unsigned int idx_left_start    = 0;
     unsigned int idx_left_end      = left_range;
     unsigned int idx_forward_start = left_range + 1;
@@ -75,19 +76,35 @@ void process_image_callback(const sensor_msgs::Image img)
     
     float x = 0.0;
     float z = 0.0;
+  
+    float offset_accumulated = 0;
+    int count_total = 0;
 
-
+/*
     // Loop through each pixel
     //   Outer loop height from top to botton
     //       Inner loop step from left to right
     //           Identify pixel is a bright one or not
     //           Increase counter in the white pixel counter vector
+*/
+  
+    // We will loop through all the pixel from left to right then top to bottom.
+    // Outer loop will be height direction, inner loop will be step direction.
+    // Assuming the ball is symmetry, we can calculate the offset from the center 
+    // of picture to the center of the ball by following approach.
+    // 1. White pixel detected, calculate the offset from the center of the picture
+    // 2. Accumulate those offset and count how many pixel detected.
+    // 3. Loop through all the pixel then divide accumulated offset by total counts
+    // 4. You get an approximate offset from the center of the picture
+  
     for (int i = 0; i < height ; i++) {
-		for (int j = 0; j < step; j++) {
-			if (img.data[i * step + j] == white_pixel) {
-				vec_cout[j]++;
-			}
-		}
+        for (int j = 0; j < step; j++) {
+            if (img.data[i * step + j] == white_pixel) {
+                // vec_cout[j]++;
+                offset_accumulated += j - step / 2.0;
+                count_total++;
+            }
+        }
     }
 
 /*
@@ -98,6 +115,7 @@ void process_image_callback(const sensor_msgs::Image img)
     cout_right   = accumulate(vec_cout + idx_right_start,   vec_cout + idx_right_end,   init);
 */
 
+/*
     // Sum up the white pixel counter vector
     int sum = 0;
     sum = accumulate(vec_cout, vec_cout + step, 0);
@@ -109,23 +127,23 @@ void process_image_callback(const sensor_msgs::Image img)
     // Calculate the center index with first and last maximum value in the vector
     // This will be the estimated center of the white ball
     unsigned int idx_center_ball = (idx_first_max + idx_last_max) / 2.0 + 1;
-  
+*/
+
     // Base on the result, determine the action
     // If there is no pixel, then tell the car to stop
-    if (sum == 0) {
+    if (count_total == 0) {
         x = 0.0;
         z = 0.0;
-	}
-	else {
+    }
+    else {
         x = 0.1;
-        z = 0.5 * (step / 2.0 - idx_center_ball);
+        // z = 0.5 * (step / 2.0 - idx_center_ball);
 
-        cout << "idx_first_max = " << idx_first_max << endl;
-        cout << "idx_last_max = " << idx_last_max << endl;
-        cout << "idx_center_ball = " << idx_center_ball << endl;
-        cout << "step = " << step << endl;
-        cout << "z = " << z << endl;
-	}
+        // Calculate the average offset (from -step/2.0 to +step/2.0)
+        // Normalize the average offset (from -1.0 to 1.0)
+        // Multiply with -0.5 to turn
+        z = -0.5 * offset_accumulated / count_total / (step /2.0);
+    }
     
     // Send request to service
     drive_robot(x, z);
